a91fbf5881a6e4644956415ae71bb0448fe8959b
Hattinger04 <s.8greiderer@gmail.com>
2021-11-26 18:54:36 +0100

    working on menue

--- Python/mathsTool.py
+++ Python/mathsTool.py
@@ -1,3 +1,8 @@
+import os
+from enum import Enum
+
+# Classes
+
 class bcolors:
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -7,6 +12,14 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+class Status(Enum): 
+    No = 0
+    Yes = 1
+    WrongInput = 2
+    Exit = 3
+    
+# Big strings and global members
 
 calculatorString = """
   ______     ___       __        ______  __    __   __          ___   .___________.  ______   .______      
@@ -57,6 +70,7 @@ kombinatorikString = """
 
 optionsString = "Options: "
 
+# Functions
 
 def binarysearch(arr, target):
     left = 0
@@ -106,6 +120,10 @@ def fakultaet(product, base):
 def calculator(): 
     print("Calc")
 
+def clearConsole(command):
+    os.system(command)
+
+# Main Menue + Checks
 
 def menue(): 
     print(bcolors.OKGREEN + nameString)
@@ -116,73 +134,127 @@ def menue():
     print("3: Search value from a list")
 
     while(True):
+
         if not checkvalue(input(">> ")): 
             return 
 
 def checkvalue(userinput):
-    if(userinput == '1'): 
+    if(userinput == '1' or userinput.startswith("calc")): 
         menuecalculator()
-    elif(userinput == '2'): 
+    elif(userinput == '2' or userinput.startswith("sort")): 
         menuesortlist()
-    elif(userinput == '3'): 
+    elif(userinput == '3' or userinput.startswith("search") or userinput.startswith("suchen")): 
         menuesearchlist()
+    elif(userinput.startswith("help")): 
+        menuehelp()
+    elif(userinput == "clear" or userinput == "cls"): 
+        clearConsole(userinput)
     elif(str(userinput).startswith("exit") or str(userinput).startswith("quit")): 
         return False
     return True
 
+# Menue + Checks Calculator
+
 def menuecalculator(): 
     print(bcolors.OKBLUE + calculatorString)
-    print("1: Rechner ")
+    print("1: Calc ")
     print("2: Kombinatorik")
     print("3: Fibonacci")
+ 
     while(True):
-         if not checkcalculation(input("# ")): 
+        if not checkcalculation(input("#> ")): 
              print(bcolors.OKGREEN + "Menue: ")
              return 
 
+def checkcalculation(userinput):
+    if(userinput == '1' or userinput.startswith("calc") or userinput.startswith("rechner")): 
+        calculator()
+    elif(userinput == '2' or userinput.startswith("kombi")): 
+        checkkombinatorik()
+    elif(userinput == '3' or userinput.startswith("fibonacci")): 
+        print("Ihr Ergebnis lautet: " + str(fibonaccireihe()))
+    elif(userinput == "clear" or userinput == "cls"): 
+        clearConsole(userinput)
+    elif(userinput.startswith("help")): 
+        calculatorhelp()
+
+    elif(str(userinput).startswith("exit") or str(userinput).startswith("quit")): 
+        return False
+    return True
+
+def calculatorhelp(): 
+    print()
+    print(bcolors.WARNING + "HELP: ")
+    print()
+    print(bcolors.OKBLUE)
+
+def checkkombinatorik(): 
+    print("Fragen zu Berechnung der Formel: ")
+    frage1 = input("Ist es eine ?(0) oder eine ?(1)? ")
+    state1 = kombinatorikabfrage(frage1)
+    while(state1 == Status.WrongInput): 
+            print("Sie haben eiune falsche Eingaebe getätigt, bitte versuchen sie es erneut: ")
+            frage1 = input("Ist es eine ?(0) oder eine ?(1)? ")
+            state1 = kombinatorikabfrage(frage1)
+
+    if state1 == Status.Yes: 
+        frage2 = input("Ist es eine ?(0) oder eine ?(1)? ")
+        state2 = kombinatorikabfrage(frage2)
+        while(state2 == Status.WrongInput): 
+            print("Sie haben eiune falsche Eingaebe getätigt, bitte versuchen sie es erneut: ")
+            frage2 = input("Ist es eine ?(0) oder eine ?(1)? ")
+            state2 = kombinatorikabfrage(frage2)
+        if state2 == Status.Yes: 
+            frage3 = input("Ist es eine ?(0) oder eine ?(1)? ")
+            state3 = kombinatorikabfrage(frage3)
+        else: 
+            frage3 = input("Ist es eine ?(0) oder eine ?(1)? ")
+            state3 = kombinatorikabfrage(frage3)
+
+    else: 
+        frage2 = input("Ist es eine ?(0) oder eine ?(1)? ")
+        state2 = kombinatorikabfrage(frage2)
+        while(state2 == Status.WrongInput): 
+            print("Sie haben eiune falsche Eingaebe getätigt, bitte versuchen sie es erneut: ")
+            frage2 = input("Ist es eine ?(0) oder eine ?(1)? ")
+            state2 = kombinatorikabfrage(frage2)
+            if state2 == Status.Yes: 
+                frage3 = input("Ist es eine ?(0) oder eine ?(1)? ")
+                state3 = kombinatorikabfrage(frage3)
+            else: 
+                frage3 = input("Ist es eine ?(0) oder eine ?(1)? ")
+                state3 = kombinatorikabfrage(frage3)
+
+
+def kombinatorikabfrage(userinput):
+    if(userinput == "1"): 
+        return Status.Yes
+    elif(userinput == "0"): 
+        return Status.No
+    elif(userinput.startswith == "exit"): 
+        return Status.Exit
+    else: 
+        return Status.WrongInput
+
+# Sort Menue
 
 def menuesortlist(): 
     print(bcolors.OKBLUE + sortString)
     print()
 
+# Search Menue
 
 def menuesearchlist(): 
     print(bcolors.OKBLUE + searchString)
     print()
 
-def menuekombinatorik(): 
-    print(bcolors.OKCYAN + kombinatorikString)
+# Help Menue
+
+def menuehelp(): 
     print()
-    print(optionsString)
-    print("1: ")
-    print("2: ")
-    print("3: ")
-    print("4: ")
-    print("5: ")
-    print("6: ")
+    print(bcolors.WARNING + "HELP: ")
+    print()
+    print(bcolors.OKGREEN)
 
-    while(True):
-         if not checkkombinatorik(input("# ")): 
-             print(bcolors.OKBLUE + "Calculatormenue: ")
-             return 
-
-
-
-def checkcalculation(userinput):
-    if(userinput == '1'): 
-        calculator()
-    elif(userinput == '2'): 
-        menuekombinatorik()
-    elif(userinput == '3'): 
-        fibonaccireihe()
-    elif(str(userinput).startswith("exit") or str(userinput).startswith("quit")): 
-        return False
-    return True
     
-def checkkombinatorik(userinput): 
-    print(int(userinput))
-
-
-
-
 menue()
